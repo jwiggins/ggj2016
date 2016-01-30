aef8b2a8b4efa10308a4ad1d83fccaf43433779e
John Wiggins <jwiggins@enthought.com>
2016-01-30 11:02:19 +0100

    Add a very simple World object to the scene.

---
+++ Automata/Assets/World.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+
+	Camera m_Camera;
+
+	// Use this for initialization
+	void Start () {
+		m_Camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
