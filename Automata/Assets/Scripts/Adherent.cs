0aedb5a18cc2ce6dc4dd6bc11be0ab260c862b5b
Barokoli <sebastian@Sebastians-MacBook-Pro.local>
2016-01-30 12:01:00 +0100

    AdherentPrefab

---
+++ Automata/Assets/Scripts/Adherent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Adherent : MonoBehaviour {
+
+	public Follower follower;
+	public Path path;
+
+	private float x = 0;
+
+	// Use this for initialization
+	void Start () {
+		follower = transform.GetChild (0).GetComponent<Follower>();
+		path = transform.GetChild (1).GetComponent<Path>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		follower.place (new Vector2(x,0),0);
+		x += 0.1f;
+	}
+		
+}
