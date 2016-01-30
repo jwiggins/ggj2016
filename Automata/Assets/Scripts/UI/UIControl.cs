d8d4ec7aec05b9f40090721c9c5b665f72082c5a
Thomas Friedel <tomee6@gmail.com>
2016-01-30 17:17:33 +0100

    added a preview for paths

--- Automata/Assets/Scripts/UI/UIControl.cs
+++ Automata/Assets/Scripts/UI/UIControl.cs
@@ -6,10 +6,13 @@ public class UIControl : MonoBehaviour {
 
 	private int uiState = 0;
 	private Vector2[] editingPoints;
+    private bool editing = false;
 	private UIObject wipObject;
 
-	public GameObject worldContainer;
-	public static World world;
+    public GameObject worldContainer;
+    public PathObject pathPreview;
+    private Renderer pathPreviewRenderer;
+    public static World world;
 
 	private Vector2 mousePos;
 
@@ -17,21 +20,33 @@ public class UIControl : MonoBehaviour {
 	void Start () {
 		world = worldContainer.GetComponent<World> ();
 		editingPoints = new Vector2[2]{new Vector2(-1,-1),new Vector2(-1,-1)};
-	}
+
+        pathPreviewRenderer = pathPreview.GetComponent<Renderer>();
+        pathPreviewRenderer.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		mousePos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
-		if (Input.GetKeyDown (KeyCode.Mouse0) && uiState != (int)uiStates.None && editingPoints[0].x == -1) {
+        mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        if (Input.GetKeyDown (KeyCode.Mouse0) && uiState != (int)uiStates.None && !editing) {
 			editingPoints[0] = mousePos;
-		}
-		if (Input.GetKeyUp (KeyCode.Mouse0) && editingPoints[1].x == -1 && editingPoints[0].x != -1) {
-			editingPoints [1] = mousePos;
-			wipObject.toAdherent(world.Add(new Vector3(editingPoints[0].x,editingPoints[0].y,0)),editingPoints);
+            editing = true;
+            pathPreviewRenderer.enabled = true;
+        }
+		if (Input.GetKeyUp (KeyCode.Mouse0) && editing) {
+            editingPoints[1] = mousePos;
+            wipObject.toAdherent(world.Add(new Vector3(editingPoints[0].x,editingPoints[0].y,0)),editingPoints);
 			uiState = (int)uiStates.None;
 			editingPoints = new Vector2[2]{new Vector2(-1,-1),new Vector2(-1,-1)};
-		}
-	}
+            editing = false;
+            pathPreviewRenderer.enabled = false;
+        }
+        if (editing) {
+            editingPoints[1] = mousePos;
+            pathPreview.transform.position = new Vector3(editingPoints[0].x, editingPoints[0].y, 0);
+            pathPreview.setShape(Path.createRect(0, 0, editingPoints[1].x - editingPoints[0].x, editingPoints[1].y - editingPoints[0].y));
+        }
+    }
 
 	public void rectangleCreator(){
 		uiState = (int)uiStates.createRectangle;
