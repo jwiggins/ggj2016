bc2fc95f3113786afdf94b21ae4a4ab4edaae142
John Wiggins <jwiggins@enthought.com>
2016-02-11 21:09:50 +0100

    Not sure why that failed.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -3,5 +3,7 @@
 echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
 curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
 
+echo ${ls}
+
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package ./Unity.pkg -target /
