3abf95a1377ff4954fc4c090e59fb46e5087ec9f
John Wiggins <jwiggins@enthought.com>
2016-02-23 22:32:27 +0100

    Complete the simple 2D animation functionality.
    Now animations can be added to Sink sprites.

--- Automata/Assets/Scripts/SimpleAnimation.cs
+++ Automata/Assets/Scripts/SimpleAnimation.cs
@@ -3,25 +3,35 @@ using System.Collections;
 
 public class SimpleAnimation : MonoBehaviour {
     [System.Serializable]
-    public class _Animation {
+    public class Anim {
         public float frameDuration;
         public Sprite[] frames;
         public bool loop;
     }
 
-    _Animation m_anim;
+    public Anim [] animations;
+
+    SpriteRenderer m_renderer;
+    Anim m_anim;
     bool m_animationRunning;
     float m_currentFrameTime;
     int m_currentFrameIndex;
 
     void Start() {
+        m_renderer = GetComponent<SpriteRenderer>();
         m_currentFrameTime = 0f;
         m_currentFrameIndex = 0;
         m_animationRunning = false;
+        if (animations.Length > 0) {
+            m_anim = animations[0];
+        }
+        else {
+            m_anim = null;
+        }
     }
 
     void Update() {
-        if (!m_animationRunning) return;
+        if (!m_animationRunning || m_anim == null) return;
 
         m_currentFrameTime += Time.deltaTime;
 
@@ -38,5 +48,17 @@ public class SimpleAnimation : MonoBehaviour {
                 m_animationRunning = false;
             }
         }
+
+        m_renderer.sprite = m_anim.frames[m_currentFrameIndex];
+    }
+
+    public void Play() {
+        m_currentFrameTime = 0f;
+        m_currentFrameIndex = 0;
+        m_animationRunning = true;
+    }
+
+    public void StopPlayback() {
+        m_animationRunning = false;
     }
 }
